cd1deddd7cea6857150d477fef2f07fb460cdc43
dmusr1002 <142683729+dmusr1002@users.noreply.github.com>
2024-05-01 08:41:00 -0400

    removing public access

--- s3.tf
+++ s3.tf
@@ -2,7 +2,6 @@ resource "aws_s3_bucket" "dmusr_bucket" {
   bucket = "dmusrbucket"
 }
 
-resource "aws_s3_bucket_public_access_block" "vulnerable_example" {
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
   bucket = aws_s3_bucket.dmusr_bucket.id
-  block_public_policy = true
 }
