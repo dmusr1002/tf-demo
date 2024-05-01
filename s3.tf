b7c0910c2bb2440ef01957821c5d4c6f4051f295
dmusr1002 <142683729+dmusr1002@users.noreply.github.com>
2024-04-30 22:34:44 -0400

    removing S3 policy

--- s3.tf
+++ s3.tf
@@ -4,5 +4,4 @@ resource "aws_s3_bucket" "dmusr_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "vulnerable_example" {
   bucket = aws_s3_bucket.dmusr_bucket.id
-  block_public_policy = true
 }
