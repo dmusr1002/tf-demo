c832759672a2b754b7b8c9769a6f2e82531e4916
dmusr1002 <142683729+dmusr1002@users.noreply.github.com>
2024-05-01 21:50:27 -0400

    adding s3 policy

--- s3.tf
+++ s3.tf
@@ -5,4 +5,5 @@ resource "aws_s3_bucket" "dmusr_bucket" {
 resource "aws_s3_bucket_public_access_block" "block_public_access" {
   bucket = aws_s3_bucket.dmusr_bucket.id
   block_public_policy = true
+  block_public_acls = true
 }
